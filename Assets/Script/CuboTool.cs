eafb9d2b56f70ec59cba13092c329aa5dc846e3e
Lobinux <lobinux@digilearnnials.com>
2021-09-13 22:46:33 -0300

    Visualizacion del cubo completo

--- Assets/Script/CuboTool.cs
+++ Assets/Script/CuboTool.cs
@@ -5,43 +5,71 @@ using UnityEngine;
 
 public class CuboTool : MonoBehaviour
 {
-    [SerializeField] Color connectionColor = default;
+    [SerializeField] Color firstVectorColor = Color.red;
+    [SerializeField] Color secondVectorColor = Color.green;
+    [SerializeField] Color thirdVectorColor = Color.blue;
+    [SerializeField] Color firstAddVectorColor = Color.magenta;
+    [SerializeField] Color secondAddVectorColor = Color.white;
+    [SerializeField] Color crossVectorColor = Color.yellow;
+    [SerializeField] Color angleColor = Color.grey;
+    [SerializeField] Color connectionColor = Color.cyan;
     [SerializeField] float radius = .1f;
-    [SerializeField] Vector3[] points = new Vector3[3];
+    [SerializeField] Vector3[] points = new Vector3[8];
 
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position, radius);
+        points[3] = points[0] + points[1] + points[2];
+        points[4] = points[0] + Vector3.Cross(points[1], points[2]);
+        points[5] = points[4] + points[1];
+        points[6] = points[2] + points[4];
+        points[7] = points[2] + points[5];
 
+        Gizmos.color = firstVectorColor;
+        Gizmos.DrawSphere(points[0], radius);
 
-        Gizmos.color = Color.blue;
+        Gizmos.color = secondVectorColor;
         Gizmos.DrawSphere(points[1], radius);
 
-        Gizmos.color = Color.blue;
+        Gizmos.color = thirdVectorColor;
         Gizmos.DrawSphere(points[2], radius);
 
-        
-        Handles.color = Color.cyan;
-        points[3] = Vector3.Cross(points[1], points[2]);
+        Gizmos.color = firstAddVectorColor;
         Gizmos.DrawSphere(points[3], radius);
 
-        Handles.DrawSolidArc(points[0], points[3], points[2], Vector3.Angle(points[0], points[1]), 1);
+        Gizmos.color = crossVectorColor;
+        Gizmos.DrawSphere(points[4], radius);
 
-        ColorUtility.TryParseHtmlString("#3900AB", out Color color);
+        Gizmos.color = secondAddVectorColor;
+        Gizmos.DrawSphere(points[5], radius);
 
-        Gizmos.color = color;
-        Gizmos.DrawSphere(points[1], radius);
+        Gizmos.color = secondAddVectorColor;
+        Gizmos.DrawSphere(points[6], radius);
 
-        points[3] = points[0] + points[1];
-        Gizmos.DrawSphere(points[3], radius);
+        Gizmos.color = secondAddVectorColor;
+        Gizmos.DrawSphere(points[7], radius);
 
-        for (int i = 0; i < points.Length; i++)
-        {
-            Gizmos.color = connectionColor;
-            Gizmos.DrawLine(transform.position, points[i]);
-        }
+        Handles.color = angleColor;
+        Handles.DrawSolidArc(points[0], points[4], points[1], Vector3.Angle(points[1], points[2]), 1);
+
+        Gizmos.color = connectionColor;
+        Gizmos.DrawLine(points[0], points[1]);
+        Gizmos.DrawLine(points[1], points[3]);
+        Gizmos.DrawLine(points[3], points[2]);
+        Gizmos.DrawLine(points[2], points[0]);
+
+        Gizmos.DrawLine(points[0], points[2]);
+        Gizmos.DrawLine(points[2], points[6]);
+        Gizmos.DrawLine(points[6], points[4]);
+        Gizmos.DrawLine(points[4], points[0]);
+
+        Gizmos.DrawLine(points[1], points[3]);
+        Gizmos.DrawLine(points[3], points[7]);
+        Gizmos.DrawLine(points[7], points[5]);
+        Gizmos.DrawLine(points[5], points[1]);
+
+        Gizmos.DrawLine(points[4], points[5]);
+        Gizmos.DrawLine(points[6], points[7]);
     }
 #endif
 }

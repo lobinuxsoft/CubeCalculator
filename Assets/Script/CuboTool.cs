ea7017910150396eaaf3c440defb94bc024c6c8f
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2021-09-16 20:50:22 -0300

    Arreglo del Codigo / Calculo de Area

--- Assets/Script/CuboTool.cs
+++ Assets/Script/CuboTool.cs
@@ -10,9 +10,46 @@ public class CuboTool : MonoBehaviour
     [SerializeField] int fontSize = 5;
     [SerializeField] Color connectionColor = Color.cyan;
     [SerializeField] float radius = .1f;
-    [SerializeField] float heightCutPoint = .3f;
+    [SerializeField] float heightCutPoint = .2f;
     [SerializeField] float planeSize = 1f;
-    [SerializeField] Vector3[] points = new Vector3[8];
+    [SerializeField] Vector3[] points = new Vector3[11];
+
+    Vector3 PointCutCalculator(Vector3 begin, Vector3 end, float height)
+    {
+        Vector3 cutPoint;
+        Vector3 vector = end - begin;
+        Vector3 point = end;
+        float landa = (height - point.y) / vector.y;
+        cutPoint.x = point.x + vector.x * landa;
+        cutPoint.z = point.z + vector.z * landa;
+        cutPoint.y = height;
+        return cutPoint;
+    }
+
+    float AreaCalculator(Vector3 point0, Vector3 point1, Vector3 point2, float height)
+    {
+        const int size = 3;
+        Vector3[] pointsForBases = new Vector3[size];
+        float[] bases = new float[size];
+        pointsForBases[0] = point0;
+        pointsForBases[1] = point1;
+        pointsForBases[2] = point2;
+        int k = 0;
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = i + 1; j < size; j++)
+            {
+                bases[k] = (pointsForBases[i] - pointsForBases[j]).magnitude;
+                k++;
+            }
+        }
+        float area = 0;
+        for (int i = 0; i < size; i++)
+        {
+            area += ((bases[i] * height) / 2);
+        }
+        return area;
+    }
 
 #if UNITY_EDITOR
     private void OnDrawGizmos()
@@ -27,6 +64,9 @@ public class CuboTool : MonoBehaviour
         points[5] = points[4] + points[1];
         points[6] = points[2] + points[4];
         points[7] = points[2] + points[5];
+        points[8] = PointCutCalculator(points[0], points[4], heightCutPoint);
+        points[9] = PointCutCalculator(points[0], points[1], heightCutPoint);
+        points[10] = PointCutCalculator(points[0], points[2], heightCutPoint);
 
         // Punto de corte
         Vector3 cutPoint = Vector3.up * heightCutPoint;
@@ -40,7 +80,7 @@ public class CuboTool : MonoBehaviour
         // Grafico punto de corte
         Handles.color = Color.black;
         Handles.SphereHandleCap(0, cutPoint, Quaternion.identity, radius, EventType.Repaint);
-        Handles.Label(cutPoint + Camera.current.transform.right * .25f, $"X={cutPoint.x:0.00} \n Y={cutPoint.y:0.00} \n Z={cutPoint.z:0.00}");
+        Handles.Label(cutPoint + Camera.current.transform.right * .1f, $"X={cutPoint.x:0.00} \n Y={cutPoint.y:0.00} \n Z={cutPoint.z:0.00} \n Area={AreaCalculator(points[8], points[9], points[10],heightCutPoint)}");
         Handles.DrawWireCube(cutPoint, new Vector3(planeSize, 0, planeSize));
         Handles.DrawLine(points[0], cutPoint, .01f);
 
@@ -49,11 +89,11 @@ public class CuboTool : MonoBehaviour
             style.normal.textColor = gradient.Evaluate((float)i / (points.Length - 1));
             Handles.color = gradient.Evaluate((float)i / (points.Length - 1));
             Handles.SphereHandleCap(0, points[i], Quaternion.identity, radius, EventType.Repaint);
-            Handles.Label(points[i] + Camera.current.transform.right * .25f, $"X={points[i].x:0.00} \n Y={points[i].y:0.00} \n Z={points[i].z:0.00}");
+            Handles.Label(points[i] + Camera.current.transform.right * .1f, $"X={points[i].x:0.00} \n Y={points[i].y:0.00} \n Z={points[i].z:0.00}");
         }
 
-        Handles.color = gradient.Evaluate((float)7 / (points.Length - 1));
-        Handles.DrawSolidArc(points[0], points[4], points[1], Vector3.Angle(points[1], points[2]), .25f);
+        //Handles.color = gradient.Evaluate((float)7 / (points.Length - 1));
+        //Handles.DrawSolidArc(points[0], points[4], points[1], Vector3.Angle(points[1], points[2]), .25f);
 
         Handles.color = connectionColor;
         Handles.DrawLine(points[0], points[1]);
@@ -73,6 +113,11 @@ public class CuboTool : MonoBehaviour
 
         Handles.DrawLine(points[4], points[5]);
         Handles.DrawLine(points[6], points[7]);
+
+        Handles.color = Color.red;
+        Handles.DrawLine(points[8], points[0]);
+        Handles.DrawLine(points[9], points[0]);
+        Handles.DrawLine(points[10], points[0]);
     }
 #endif
 }

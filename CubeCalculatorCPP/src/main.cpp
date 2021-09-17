3e468b739210ee0cc3834376dbd7352cb8db8d3d
Lobinux <lobinux@digilearnnials.com>
2021-09-16 21:30:40 -0300

    Transformado el proyecto a C++

---
+++ CubeCalculatorCPP/src/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include "Vector3.h"
+
+Vector3 pointCutCalculator(Vector3 begin, Vector3 end, float height);
+
+void main()
+{
+	const int pointsAmount = 11;
+	float heightCutPoint = 0.2f;
+    Vector3 points[pointsAmount];
+
+    for (int i = 0; i < pointsAmount; i++)
+    {
+        points[i] = Vector3{ 0,0,0 };
+    }
+
+    points[0] = Vector3Normalize(points[0]);
+    points[1] = Vector3Normalize(points[1]);
+    points[2] = Vector3Normalize(points[2]);
+    points[3] = points[0] + points[1] + points[2];
+    points[4] = points[0] + Vector3Normalize((Vector3CrossProduct(points[1], points[2])));
+    points[5] = points[4] + points[1];
+    points[6] = points[2] + points[4];
+    points[7] = points[2] + points[5];
+    points[8] = pointCutCalculator(points[0], points[4], heightCutPoint);
+    points[9] = pointCutCalculator(points[0], points[1], heightCutPoint);
+    points[10] = pointCutCalculator(points[0], points[2], heightCutPoint);
+}
+
+Vector3 pointCutCalculator(Vector3 begin, Vector3 end, float height)
+{
+	Vector3 cutPoint = Vector3{ 0,0,0 };
+	Vector3 vector = end - begin;
+	Vector3 point = end;
+	float landa = (height - point.y) / vector.y;
+	cutPoint.x = point.x + vector.x * landa;
+	cutPoint.z = point.z + vector.z * landa;
+	cutPoint.y = height;
+	return cutPoint;
+}
+
+float AreaCalculator(Vector3 point0, Vector3 point1, Vector3 point2, float height)
+{
+    const int size = 3;
+    Vector3 pointsForBases[size];
+    float bases[size];
+    pointsForBases[0] = point0;
+    pointsForBases[1] = point1;
+    pointsForBases[2] = point2;
+    int k = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            bases[k] = (pointsForBases[i] - pointsForBases[j]).Magnitude();
+            k++;
+        }
+    }
+    float area = 0;
+    for (int i = 0; i < size; i++)
+    {
+        area += ((bases[i] * height) / 2);
+    }
+    return area;
+}
